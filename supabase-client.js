9b5e27a79195768397dd8dde4c88fcdc9985bb9c
Lemaxoo <lemaxoot@gmail.com>
2026-08-16 17:13:04 -0500

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,5 +1,5 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from './supabase/config.js';
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from './supabase/config.js?v=2';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
