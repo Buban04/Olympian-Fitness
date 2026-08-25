bf9f5075399e9b86529894ea063a34058aca0683
Lemaxoo <lemaxoot@gmail.com>
2026-08-24 20:37:48 -0500

    Update SUPABASE_URL to remove '/rest/v1/'

--- supabase/config.js
+++ supabase/config.js
@@ -1,2 +1,2 @@
-export const SUPABASE_URL = "https://jleqvhgrawuezzvikddo.supabase.co/rest/v1/";
+export const SUPABASE_URL = "https://jleqvhgrawuezzvikddo.supabase.co";
 export const SUPABASE_ANON_KEY = "sb_publishable_JokhHy2FD2zOkfl1ijo3iA_vinN_QLn";
